09f06c8fe0155c5b4c193823101ef3d0b916b42a
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-05-19 08:50:49 +0700

    fcmt

---
+++ app/src/main/java/com/example/firebase/SignupActivity.java
@@ -0,0 +1,26 @@
+package com.example.firebase;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class SignupActivity extends AppCompatActivity {
+
+}
